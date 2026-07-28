f63584e5e60e52001a2598c3b636b1ec4da3e66e
Tapasmitha-Vechalapu <tapasmitha.vechalapu18@gmail.com>
2026-07-28 10:35:19 +0000

    js done

---
+++ js/script.js
@@ -0,0 +1,4 @@
+alert("welcome to NRIIT  learning management system")
+let heading = document.getElementById("welcome");
+heading.innerHTML = "welcome future software engineers"
+console.log("Heading element:",heading)
---
+++ js/test.js
@@ -0,0 +1,30 @@
+console.log("hello world")
+let student_name = "sriram"
+console.log("Student name is:"+ student_name)
+let student_age = 21
+let course_name ="python full stack"
+console.log("student age is :"+ student_age)
+console.log("course name is :"+course_name)
+let age =18
+if(age>=18){
+    console.log("Eligible for admission")
+}
+else{
+    console.log("not eligible for admission")
+}
+for(let i=1; i<=5; i++){
+    console.log("Iteration number:" + i)
+}
+student = {
+    name: "sriram",
+    age:21,
+    course:"python full stack",
+
+}
+console.log("student details:",student)
+
+
+function greetStudent(name){
+    console.log("hello"+name+"welcome to nriit learning management system")
+}
+greetStudent(student_name)
