865a4efba0f7a3ae71f4c00b0945b5d4261ce715
Tapasmitha-Vechalapu <tapasmitha.vechalapu18@gmail.com>
2026-08-04 10:40:55 +0000

    folder update

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template("index.html")
 @app.route("/about")
@@ -13,8 +13,21 @@ def courses():
     return render_template("courses.html")
 @app.route("/trainer")
 def trainer():
-    return render_template("trainer.html")         
+    return render_template("trainer.html")  
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")  
+@app.route("/register",methods=["post","get"])
+def register():
+    if request.method=="post":
+        name=request.form["name"]
+        email=request.form["email"] 
+        password=request.form["password"]
+        dob=request.form["dob"]
+        gender=request.form["gender"]
+        courses=request.form["courses"]
+        return render_template("register.html")            
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
