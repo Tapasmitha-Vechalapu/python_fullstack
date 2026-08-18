0dbf8a5f50eca847930691ac819bee2e200ea810
Tapasmitha-Vechalapu <tapasmitha.vechalapu18@gmail.com>
2026-08-18 10:15:47 +0000

    login

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sqlite3
 # pyrefly: ignore [missing-import]
 from flask import Flask,render_template,jsonify,request,redirect,url_for,session
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key="super_secret_key"
@@ -28,13 +28,14 @@ def init_db():
         course TEXT NOT NULL
     )
     """)
+    # create tasks table
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS tasks(
+    CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_emai TEXT NOT NULL,
+        user_email TEXT NOT NULL,
         title TEXT NOT NULL,
-        status TEXT DEFAULT 'pending'
-        ) 
+        status TEXT DEFAULT 'Pending'
+    )
     """)
     conn.commit()
     conn.close()
@@ -79,7 +80,8 @@ def api_register():
     if user:
         return jsonify({"status": "error", "message": "User already exists with this email!"}), 400
     
-    cursor.execute("INSERT INTO users (name, email, password, dob, gender, course) VALUES (?, ?, ?, ?, ?, ?)", (data["name"], data["email"], data["password"], data["dob"], data["gender"], data["course"]))
+    hashed_password = generate_password_hash(data["password"])
+    cursor.execute("INSERT INTO users (name, email, password, dob, gender, course) VALUES (?, ?, ?, ?, ?, ?)", (data["name"], data["email"], hashed_password, data["dob"], data["gender"], data["course"]))
     conn.commit()
     conn.close()
     return jsonify({"status": "success", "message": "Registration successful!"})
@@ -94,71 +96,75 @@ def api_login():
     cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
     user = cursor.fetchone()
     conn.close()
-    if user and user["password"] == password:
+    if user and check_password_hash(user["password"], password):
+        # session variables to keep track of logged in user
         session["user_email"] = user["email"]
+        session["user_name"] = user["name"]
         return jsonify({"status": "success", "message": "Login successful! Welcome back."})
     else:
         return jsonify({"status": "error", "message": "Invalid email or password!"}), 401
 @app.route('/logout', methods=["GET"])
 def logout():
-    session.pop("user_email",None)
-    session.pop("user_name",None)
+    session.pop("user_email", None)
+    session.pop("user_name", None)
     return redirect(url_for("login"))
+
 @app.route('/tasks',methods=["GET","POST"])
-def get_tasks():
-    user_email = session.get("user_email")
-    if not user_email:
-        return jsonify ({ "status":"error","message":"No user logged in!,login please! "}),
-        401
-    return render_template("tasks.html")  
+def tasks():
+    return render_template("tasks.html")
 @app.route('/api/tasks',methods=["GET"])
 def get_tasks():
+    # return tasks from database
+    user_email = session.get("user_email")
+    if not user_email:
+        return jsonify({"status":"error", "message":"No user logged in!,Login please!"}),401
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM tasks WHERE user_email=?",(user_email,))
-    tasks = cursor.fetchall() 
+    cursor.execute("SELECT * FROM tasks WHERE user_email = ?", (user_email,))
+    tasks = cursor.fetchall()
     conn.close()
-    tasks_list = [dict (row) for row in tasks]
-    return jsonify({"status":"success","tasks":tasks_list})
+    task_list = [dict(row) for row in tasks]
+    return jsonify({"status": "success", "tasks": task_list})
 @app.route('/api/tasks',methods=['POST'])
 def add_tasks():
+    # add tasks to database
+    user_email = session.get("user_email")
     if not user_email:
-        return jsonify ({ "status":"error","message":"No user logged in!,login please! "}),
-        401
+        return jsonify({"status":"error", "message":"No user logged in!,Login please!"}),401
     data = request.get_json()
     title = data.get("title")
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO tasks (user_email, title) VALUES  (?,?)", (user_email,title))
+    cursor.execute("INSERT INTO tasks (user_email, title) VALUES (?, ?)", (user_email, title))
     conn.commit()
     conn.close()
-    return jsonify({"status": "success", "message":"Tasks added successfully!"})
-@app.route('/api/tasks/<int:tasks_id>',methods=['PUT'])
+    return jsonify({"status": "success", "message": "Task added successfully!"})
+@app.route('/api/tasks/<int:task_id>',methods=['PUT'])
 def update_tasks(task_id):
     user_email = session.get("user_email")
     if not user_email:
-        return jsonify({"status":"error","message":"NO user logged in!, login please!"}),
-        401
+        return jsonify({"status":"error", "message":"No user logged in!,Login please!"}),401
     data = request.get_json()
     status = data.get("status")
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("UPDATE TASKS SET status = ? WHERE id= ? ",(status,tasks  id ))
+    cursor.execute("UPDATE tasks SET status = ? WHERE id = ?", (status, task_id))
     conn.commit()
     conn.close()
-    return jsonify({"status":"success","message":"Task update successfully!"})
+    return jsonify({"status": "success", "message": "Task updated successfully!"})
 @app.route('/api/tasks/<int:task_id>',methods=['DELETE'])
 def delete_tasks(task_id):
-    user_email=sesion.get("user_email")
+    user_email=session.get("user_email")
     if not user_email:
-        return jsonify({"status":"error","message":"NO user logged in!,login please!"}),
-        401 
-    conn = get_db_connection()
+        return jsonify({"status":"error", "message":"No user logged in!,Login please!"}),401
+    conn=get_db_connection()
     cursor=conn.cursor()
-    cursor.execute("DELETE FROM tasks WHHERE user_email=? AND id=?",(user-email,task_id))
+    cursor.execute("DELETE FROM tasks WHERE user_email=? AND id=?",(user_email,task_id))
     conn.commit()
     conn.close()
-    return jsonify({"status":"success"})                  
-
+    return jsonify({"status":"success", "message":"Task deleted successfully!"})
+    
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
